5bb43f4fb21b4b02326f12603173e1a9332466b1
Oskari Mantere <osku.mantere@gmail.com>
2016-07-16 13:09:47 +0300

    Cleanup and urldecoding of random fucked up chars

--- index.js
+++ index.js
@@ -2,84 +2,37 @@
  * Created by oskari on 27.4.2016.
  */
 
-
-
-
 (function() {
 
-    var parser = {}
+    var fs = require('fs');
+    var PDFParser = require('pdf2json');
+    var pdfParser = new PDFParser();
+
+    const parseHelpers = require('./lib/parse-helpers');
 
 
-    var getTextItems = function (object) {
+    function parse(pdfFile) {
 
-        var out = []
+        //For testing
+        pdfFile = 'enk2016.pdf';
 
-        if(object instanceof Array) {
-            for(var i in object) {
-                out = out.concat(getTextItems(i))
-            }
-        }
-
-        if (typeof object == "object") {
-            for (var property in object) {
-                if (object.hasOwnProperty(property)) {
-                    if (typeof object[property] == "object") {
-                        out = out.concat(getTextItems(object[property]));
-                    } else if (object[property]) {
-                        if (property == "T" && !object[property].match('^[0-9]+$')) {
-                            out.push(object[property]);
-                            console.log(object);
-                        }
-                    }
-                }
-            }
-        }
-
-        return out
-    }
-
-
-    exports.parseFromUrl = parser.parseFromUrl = function (url) {
-
-        var http = require('https');
-        var fs = require('fs');
-        var PDFParser = require('pdf2json')
-
-        var pdfParser = new PDFParser()
-        var pdfFile = 'enk2016.pdf'
-        var textFile = 'text.txt'
-
-        var textData
-        var json
+        var urlDecodedFile = 'urldecoded.txt';
+        var textData;
+        var json;
 
         pdfParser.on("pdfParser_dataReady", pdfData => {
-            json = JSON.parse(decodeURI(JSON.stringify(pdfData)))
-            textData = getTextItems(json.formImage.Pages)
-            console.log(textData)
-            console.log(textData.join())
-            //const outJson = processKirjallinen(textData);
-            fs.writeFile("./" + textFile, JSON.stringify(textData));
-            fs.writeFile("./" + 'puretext.txt', textData.join('\n'));
+            json = JSON.parse(decodeURI(JSON.stringify(pdfData)));
+            textData = parseHelpers.getTextItems(json.formImage.Pages);
+            fs.writeFile("./" + urlDecodedFile, textData.map(str => parseHelpers.correctUrlChars(str)).join('\n'));
         });
 
         pdfParser.loadPDF(pdfFile)
-
-
-        /*var request = http.get(url, function (response) {
-            console.log(response)
-            fs.writeFile(pdfFile, response, function (err) {
-                if(err)
-                    console.log('Error writing temp pdf')
-                else {
-                    pdfParser.loadPDF(pdfFile)
-                }
-            })
-            
-        })*/
     }
 
+    module.exports = parse;
+
     if (!module.parent) {
-        parser.parseFromUrl(process.argv[2])
+        parse(process.argv[2])
     }
 
 })();
