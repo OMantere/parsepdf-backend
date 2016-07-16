81bb275cbdd1b70d0d740b41bab036c7b9f7d4c4
Oskari Mantere <osku.mantere@gmail.com>
2016-07-16 15:32:24 +0300

    Questions and a couple of headers parsed succesfully, basic JSON structure generation

--- index.js
+++ index.js
@@ -9,12 +9,13 @@
     var pdfParser = new PDFParser();
 
     const parseHelpers = require('./lib/parse-helpers');
+    const enLongParser = require('./lib/parsers/english-long');
 
 
-    function parse(pdfFile) {
+    function parse(file) {
 
         //For testing
-        pdfFile = 'enk2016.pdf';
+        file = 'enk2014';
 
         var urlDecodedFile = 'urldecoded.txt';
         var textData;
@@ -23,10 +24,12 @@
         pdfParser.on("pdfParser_dataReady", pdfData => {
             json = JSON.parse(decodeURI(JSON.stringify(pdfData)));
             textData = parseHelpers.getTextItems(json.formImage.Pages);
-            fs.writeFile("./" + urlDecodedFile, textData.map(str => parseHelpers.correctUrlChars(str)).join('\n'));
+
+            fs.writeFile("./" + urlDecodedFile, textData.join('\n'));
+            fs.writeFile("./" + file + '.json', enLongParser(textData));
         });
 
-        pdfParser.loadPDF(pdfFile)
+        pdfParser.loadPDF(file + '.pdf')
     }
 
     module.exports = parse;
